f5a68d8282d730887e18be609f84f519e65e6e70
cjhdev <cam@cjh.id.au>
2016-07-11 13:45:52 +0100

    added terminal output

---
+++ examples/working_with_data/terminal/decode.rb
@@ -0,0 +1,33 @@
+DType:
+#<DLMSTrouble::DCompactArray:0x00000000de3d98
+ @value=
+  [#<DLMSTrouble::DStructure:0x00000000de3028
+    @value=
+     [#<DLMSTrouble::DDateTime:0x00000000de2808
+       @dom=10,
+       @dow=7,
+       @gmt_offset=60,
+       @hour=8,
+       @hun=0,
+       @min=0,
+       @month=7,
+       @sec=0,
+       @status=[:invalid_status],
+       @year=2016>,
+      #<DLMSTrouble::DDoubleLongUnsigned:0x00000000de2010 @value=0>,
+      #<DLMSTrouble::DDoubleLongUnsigned:0x00000000de1cc8 @value=810>]>,
+   #<DLMSTrouble::DStructure:0x00000000de1a48
+    @value=
+     [#<DLMSTrouble::DDateTime:0x00000000de1458
+       @dom=10,
+       @dow=7,
+       @gmt_offset=60,
+       @hour=8,
+       @hun=0,
+       @min=30,
+       @month=7,
+       @sec=0,
+       @status=[:invalid_status],
+       @year=2016>,
+      #<DLMSTrouble::DDoubleLongUnsigned:0x00000000de0ff8 @value=1>,
+      #<DLMSTrouble::DDoubleLongUnsigned:0x00000000de0dc8 @value=820>]>]>
